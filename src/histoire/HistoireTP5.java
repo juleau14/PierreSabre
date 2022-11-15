574d615a0055cd2e3c9b87cc3495734c0cc223f6
camuzet jules <cmj4104a@ens.fsi>
2022-11-15 17:24:20 +0100

    debut partie samourai

--- src/histoire/HistoireTP5.java
+++ src/histoire/HistoireTP5.java
@@ -4,25 +4,12 @@ import personnages.*;
 public class HistoireTP5 {
 	public static void main(String[] args) {
 		 Commercant marco =  new Commercant("Marco", 20); 
-		 Commercant chonin =  new Commercant("Chonin", 40);
-		 Commercant kumi =  new Commercant("Kumi", 10);
-		 Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
-		 Ronin roro = new Ronin("Roro", "shochu", 60);
-
-		 marco.faireConnaissance(roro);
-		 roro.faireConnaissance(marco);
+		 Samourai akimoto = new Samourai("Akimoto", "Saké", 80, "Miyamoto");
 		 
-		 marco.faireConnaissance(yaku);
-		 yaku.faireConnaissance(marco);
-
-		 marco.faireConnaissance(chonin);
-		 chonin.faireConnaissance(marco);
+		 akimoto.faireConnaissance(marco);
+		 marco.faireConnaissance(akimoto);
 		 
-		 marco.faireConnaissance(kumi);
-		 kumi.faireConnaissance(marco);
-		 
-		 marco.listerConnaissance();
-		 roro.listerConnaissance();
-		 yaku.listerConnaissance();
+		 akimoto.listerConnaissance();
+		 akimoto.boire("thé");;
 	}
 }
---
+++ src/personnages/Samourai.java
@@ -0,0 +1,22 @@
+package personnages;
+
+public class Samourai extends Ronin {
+	private String seigneur;
+
+	public Samourai(String nom, String boissonFavorite, int argent, String seigneur) {
+		super(nom, boissonFavorite, argent);
+
+		this.seigneur = seigneur;
+	}
+
+	@Override
+	public void direBonjour() {
+		super.direBonjour();
+		parler("Je suis fier de servir le seigneur " + this.seigneur + ".");
+	}
+	
+	
+	public void boire(String boisson) {
+		parler("Qu'est-ce que je vais choisir comme boisson ? Tiens, je vais prendre du " + boisson + " !");
+	}
+}
